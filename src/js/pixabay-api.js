b218a7e9791a28b1e29e46bdd338a2b191ba845d
YuriiGreg <yuriyf888@gmail.com>
2024-05-30 12:01:45 +0200

    js-hw-12

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const API_KEY = '44034856-409a23e11e96e3af40ee90684';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function fetchImages(query, page = 1, perPage = 15) {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page,
+        per_page: perPage,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    throw error;
+  }
+}
+
---
+++ src/js/render-functions.js
@@ -0,0 +1,61 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+let lightbox;
+
+export function renderImages(images, gallery) {
+  const markup = images.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
+    <li>
+      <a href="${largeImageURL}">
+        <img src="${webformatURL}" alt="${tags}" />
+      </a>
+      <div class="info">
+        <p><b>Likes</b> ${likes}</p>
+        <p><b>Views</b> ${views}</p>
+        <p><b>Comments</b> ${comments}</p>
+        <p><b>Downloads</b> ${downloads}</p>
+      </div>
+    </li>
+  `).join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+
+  if (!lightbox) {
+    lightbox = new SimpleLightbox('.gallery a');
+  } else {
+    lightbox.refresh();
+  }
+}
+
+export function clearGallery(gallery) {
+  gallery.innerHTML = '';
+}
+
+export function hideLoadMoreButton() {
+  document.querySelector('.load-more').classList.add('is-hidden');
+}
+
+export function showLoadMoreButton() {
+  document.querySelector('.load-more').classList.remove('is-hidden');
+}
+
+export function displayEndOfResultsMessage() {
+  document.querySelector('.end-of-results').classList.remove('is-hidden');
+}
+
+export function hideEndOfResultsMessage() {
+  document.querySelector('.end-of-results').classList.add('is-hidden');
+}
+
+export function smoothScroll() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
+
+
